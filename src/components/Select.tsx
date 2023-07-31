1f25e65a7e993b11413d78b1d5f14901a5da048f
killu4kun <mateeus.soarees@outlook.com>
2023-07-31 13:52:01 -0300

    select

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -3,14 +3,23 @@ import React, { forwardRef } from 'react';
 interface SelectProps extends React.ComponentProps<'select'> {
   label?: string;
   options?: Array<{ value: string; label: string }>;
+  variant?: 'solid' | 'ghost'
+}
+
+const defaultStyles = "flex items-center justify-center gap-2 font-medium rounded-lg text-sm px-5 py-2.5 w-full focus:outline-violet-600"
+
+
+const selectVariants = {
+  solid: 'text-white bg-violet-600 hover:bg-violet-700 disabled:bg-neutral-200',
+  ghost: 'text-violet-600 hover:text-violet-700 hover:bg-neutral-200'
 }
 
 export const Select = forwardRef<HTMLSelectElement, SelectProps>(
-  ({ label, id, options, ...rest }, ref) => (
+  ({ label, id, options,variant='ghost', ...rest }, ref) => (
     <label htmlFor={id}>
       {label}
 
-      <select ref={ref} {...rest} id={id}>
+      <select ref={ref} {...rest} id={id} className={`${defaultStyles} ${selectVariants[variant]}`}>
         {options?.map((option) => (
           <option key={option.value} value={option.value}>
             {option.label}
--- src/pages/Calculator.tsx
+++ src/pages/Calculator.tsx
@@ -1,41 +1,24 @@
+import { Button } from '../components/Button';
 import { Input } from '../components/Input';
 import { Select } from '../components/Select';
 
 function Calculator() {
   return (
-    <div className='w-screen flex flex-col items-center'>
+    <div className='flex flex-col items-center'>
       <h1>Calculadora de unidades por página</h1>
-      <div className='flex flex-col items-center'>
-        <h3>Tamanho do material (mm)</h3>
-        <div className='flex gap-2'>
-          <Input
-            className='border-pink-700 border h-16 w-16 outline-none'
-            type='number'
-          />
-          <p>X</p>
-          <Input
-            className='border-pink-700 border h-16 w-16 outline-none'
-            type='number'
-          />
+      <div className='flex flex-col items-center mt-5'>
+        <h3 className='mb-5'>Tamanho do material (mm)</h3>
+        <div className='flex gap-2 mb-6'>
+          <Input type='number'/>
+          <div className='flex justify-center items-center'>X</div>
+          <Input type='number' />
         </div>
         <div className='flex flex-col items-center'>
           <div className='flex flex-col'>
-            <Input
-              className='border border-b-pink-700 w-full'
-              id='quantity'
-              label='Quntidade (un) '
-            />
-
-            <Select
-              label='Selecione o Papel'
-              id='paper'
-              className='border border-b-pink-700 w-full'
-            />
-            <Input
-              className='border border-b-pink-700 w-full'
-              id='costPerPage'
-              label='Custo página '
-            />
+            <Input className='w-full' id='quantity' label='Quntidade (un) ' />
+            <Select label='Selecione o Papel' id='paper' className='w-full' />
+            <Input className='w-full' id='costPerPage' label='Custo página ' />
+            <Button>Calcular</Button>
           </div>
         </div>
       </div>

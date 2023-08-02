1bae8079fd747336a4fb689fd6c6f5cc90f44c27
Kauan Carvalho <kauan.carvalho@memed.com.br>
2023-08-02 18:04:13 -0300

    ReactPortal has been added into Dialog

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,29 +1,37 @@
 import React from "react";
 
-import { GrClose } from 'react-icons/gr'
+import ReactDOM from "react-dom";
+
+import { GrClose } from "react-icons/gr";
 
 interface DialogProps {
   children: React.ReactNode;
   onClose: () => void;
 }
 
-export const Dialog = ({ children, onClose }: DialogProps) => (
-  <div
-    id="dialog_overlay"
-    className="fixed overflow-hidden mx-auto w-screen h-screen inset-0 bg-black bg-opacity-50 z-10"
-    onClick={onClose}
-  >
+export const Dialog = ({ children, onClose }: DialogProps) =>
+  ReactDOM.createPortal(
     <div
-      id="dialog_content"
-      role="dialog"
-      className="flex flex-col gap-8 w-[512px] h-max p-4 bg-white absolute inset-0 m-auto rounded-xl"
-      onClick={(event) => event.stopPropagation()}
+      id="dialog_overlay"
+      className="fixed overflow-hidden mx-auto w-screen h-screen inset-0 bg-black bg-opacity-50 z-10"
+      onClick={onClose}
     >
-      <button type="button" onClick={onClose} className="flex items-center justify-center p-2 absolute top-4 right-4 rounded-full hover:bg-neutral-200 focus:outline-violet-600">
-        <GrClose />
-      </button>
+      <div
+        id="dialog_content"
+        role="dialog"
+        className="flex flex-col gap-8 w-[512px] h-max p-4 bg-white absolute inset-0 m-auto rounded-xl"
+        onClick={(event) => event.stopPropagation()}
+      >
+        <button
+          type="button"
+          onClick={onClose}
+          className="flex items-center justify-center p-2 absolute top-4 right-4 rounded-full hover:bg-neutral-200 focus:outline-violet-600"
+        >
+          <GrClose />
+        </button>
 
-      {children}
-    </div>
-  </div>
-);
+        {children}
+      </div>
+    </div>,
+    document.body
+  );
